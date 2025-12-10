2a8ed059ab87f710a6769cbdd3684533d3d9c68c
DivyaSai20 <divyasaibolla7@gmail.com>
2025-12-10 09:17:09 +0000

    Scenario-1

--- lib/cdk-scenario-1-stack.ts
+++ lib/cdk-scenario-1-stack.ts
@@ -1,16 +1,52 @@
-import * as cdk from 'aws-cdk-lib/core';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class CdkScenario1Stack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // ---------------------------------------------------------
+    // 1️⃣ CREATE VPC
+    // ---------------------------------------------------------
+    const vpc = new ec2.Vpc(this, "CdkVPC", {
+      maxAzs: 2, // Public + private subnets in 2 AZs
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkScenario1Queue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // ---------------------------------------------------------
+    // 2️⃣ CREATE S3 BUCKET
+    // ---------------------------------------------------------
+    const bucket = new s3.Bucket(this, "MyDemoBucket", {
+      bucketName: "cdk-scenario1-1210",
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      autoDeleteObjects: true,
+    });
+
+    // ---------------------------------------------------------
+    // 3️⃣ CREATE SECURITY GROUP FOR EC2 (Allow SSH)
+    // ---------------------------------------------------------
+    const ec2SG = new ec2.SecurityGroup(this, "EC2SecurityGroup", {
+      vpc,
+      description: "Allow SSH for EC2",
+      allowAllOutbound: true,
+    });
+
+    ec2SG.addIngressRule(
+      ec2.Peer.anyIpv4(),
+      ec2.Port.tcp(22),
+      "Allow SSH access"
+    );
+
+    // ---------------------------------------------------------
+    // 4️⃣ CREATE EC2 INSTANCE
+    // ---------------------------------------------------------
+    new ec2.Instance(this, "MyEC2Instance", {
+      vpc,
+      instanceType: new ec2.InstanceType("t3.micro"), // Free tier
+      machineImage: ec2.MachineImage.latestAmazonLinux2023(),
+      securityGroup: ec2SG,
+    });
   }
 }
